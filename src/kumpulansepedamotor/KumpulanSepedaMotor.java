a5f4d9b74c72f730c49df9f087f7060741056aa7
MuhammadGagah <muha.aku@gmail.com>
2022-11-22 14:17:06 +0700

    Menambahkan semua files

---
+++ src/kumpulansepedamotor/KumpulanSepedaMotor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kumpulansepedamotor;
+
+/**
+ *
+ * @author Muhammad
+ */
+public class KumpulanSepedaMotor {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String warna = "Merah";
+        String merek = "yamaha";
+        int tahun;
+        tahun = 2010;
+        System.out.println("Fajar mempunyai motor merek " +merek+ ", warnanya "+warna +", dan motornya keluaran tahun "+tahun);
+        System.out.println("Terima kasih");
+        System.out.println("Muhammad");
+    }
+}
